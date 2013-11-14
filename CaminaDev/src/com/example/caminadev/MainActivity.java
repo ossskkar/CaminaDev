a133cefb2f337d9feacaf1baaccb7c7b53a20c1d
ossskkar <ossskkar@gmail.com>
2013-11-14 13:14:54 -0800

    create layout

--- CaminaDev/src/com/example/caminadev/MainActivity.java
+++ CaminaDev/src/com/example/caminadev/MainActivity.java
@@ -1,7 +1,8 @@
 package com.example.caminadev;
 
-import android.os.Bundle;
+
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
 
 public class MainActivity extends Activity {
@@ -11,12 +12,4 @@ public class MainActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
-
 }
